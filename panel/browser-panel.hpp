d42d06063eb4516c01403ed18bcf4862be785556
Ilya Melamed <ilya77@gmail.com>
2021-02-09 20:10:48 +0200

    Revert "Use new module function to find obs-browser lib"
    This reverts commit 5d4197f0a6f08e8c1e8a68aca7bcc7cd4306d682.

--- panel/browser-panel.hpp
+++ panel/browser-panel.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <obs-module.h>
 #include <util/platform.h>
 #include <util/util.hpp>
 #include <QWidget>
@@ -73,46 +72,36 @@ struct QCef {
 					 QObject *obj) = 0;
 };
 
-static inline void *get_browser_lib()
+static inline void *obs_browser_dlsym(const char *name)
 {
-	obs_module_t *browserModule = obs_get_module("obs-browser");
-
-	if (!browserModule)
+#if defined(_WIN32)
+	void *lib = os_dlopen("obs-browser");
+#elif defined(__APPLE__)
+	void *lib = RTLD_DEFAULT;
+#else
+	void *lib = os_dlopen("../obs-plugins/obs-browser");
+#endif
+	if (!lib) {
 		return nullptr;
+	}
 
-	return obs_get_module_lib(browserModule);
+	return os_dlsym(lib, name);
 }
 
 static inline QCef *obs_browser_init_panel(void)
 {
-	void *lib = get_browser_lib();
-	QCef *(*create_qcef)(void) = nullptr;
-
-	if (!lib)
-		return nullptr;
-
-	create_qcef =
-		(decltype(create_qcef))os_dlsym(lib, "obs_browser_create_qcef");
-
+	QCef *(*create_qcef)(void) = (decltype(create_qcef))obs_browser_dlsym(
+		"obs_browser_create_qcef");
 	if (!create_qcef)
 		return nullptr;
-
 	return create_qcef();
 }
 
 static inline int obs_browser_qcef_version(void)
 {
-	void *lib = get_browser_lib();
-	int (*qcef_version)(void) = nullptr;
-
-	if (!lib)
-		return 0;
-
-	qcef_version = (decltype(qcef_version))os_dlsym(
-		lib, "obs_browser_qcef_version_export");
-
+	int (*qcef_version)(void) = (decltype(qcef_version))obs_browser_dlsym(
+		"obs_browser_qcef_version_export");
 	if (!qcef_version)
 		return 0;
-
 	return qcef_version();
 }
