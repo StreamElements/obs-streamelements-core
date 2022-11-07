24bc029aa237b7b9117b0abbe574298694dd9c19
Ilya Melamed <ilya77@gmail.com>
2022-11-07 20:56:58 +0300

    wip

--- streamelements/StreamElementsObsOutputManager.cpp
+++ streamelements/StreamElementsObsOutputManager.cpp
@@ -1,1 +1,162 @@
+#include "StreamElementsUtils.hpp"
 #include "StreamElementsObsOutputManager.hpp"
+
+std::string StreamElementsObsOutputManager::AddSceneOutput(obs_scene_t *scene,
+							   int view_width,
+							   int view_height)
+{
+	std::lock_guard<decltype(m_mutex)> guard(m_mutex);
+
+	std::string id = CreateGloballyUniqueIdString();
+
+	m_outputs[id] = std::make_shared<StreamElementsObsOutput>(scene, view_width, view_height);
+
+	return id;
+}
+
+bool StreamElementsObsOutputManager::StartOutputById(std::string id) {
+	std::lock_guard<decltype(m_mutex)> guard(m_mutex);
+
+	if (m_outputs.count(id) == 0)
+		return false;
+
+	return m_outputs[id]->Start();
+}
+
+bool StreamElementsObsOutputManager::StopOutputById(std::string id)
+{
+	std::lock_guard<decltype(m_mutex)> guard(m_mutex);
+
+	if (m_outputs.count(id) == 0)
+		return false;
+
+	m_outputs[id]->Stop();
+
+	return true;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// StreamElementsObsOutput
+/////////////////////////////////////////////////////////////////////////////
+
+void StreamElementsObsOutput::Stop()
+{
+	std::lock_guard<decltype(m_mutex)> guard(m_mutex);
+
+	if (m_output) {
+		obs_output_stop(m_output);
+
+		obs_output_release(m_output);
+
+		m_output = nullptr;
+	}
+
+	if (m_view) {
+		obs_view_remove(m_view);
+		obs_view_destroy(m_view);
+
+		m_view = nullptr;
+		m_video = nullptr;
+	}
+}
+
+bool StreamElementsObsOutput::Start()
+{
+	std::lock_guard<decltype(m_mutex)> guard(m_mutex);
+
+	if (m_video)
+		return false;
+
+	// Output settings
+	int maxBitrateBitsPerSecond = 1500000;
+	std::string serverUrl;
+	std::string streamKey;
+	bool useAuth = false;
+	std::string authUsername;
+	std::string authPassword;
+	std::string bindToIP;
+
+	m_view = obs_view_create();
+	m_video = obs_view_add(m_view);
+
+	obs_source_t *scene_source = obs_scene_get_source(m_container_scene);
+
+	obs_view_set_source(m_view, 0, scene_source);
+
+	auto video_info = video_output_get_info(m_video);
+
+	obs_encoder_t *vencoder = obs_video_encoder_create(
+		"obs_x264", "test_x264", nullptr, nullptr);
+	obs_encoder_t *aencoder = obs_audio_encoder_create(
+		"ffmpeg_aac", "test_aac", nullptr, 0, nullptr);
+	obs_service_t *service = obs_service_create(
+		"rtmp_custom", "test_service", nullptr, nullptr);
+
+	obs_data_t *service_settings = obs_data_create();
+
+	// Configure video encoder
+	obs_data_t *vencoder_settings = obs_data_create();
+
+	obs_data_set_int(vencoder_settings, "bitrate",
+			 (int)(maxBitrateBitsPerSecond / 1000L));
+	obs_data_set_string(vencoder_settings, "rate_control", "CBR");
+	obs_data_set_string(vencoder_settings, "preset", "veryfast");
+	obs_data_set_int(vencoder_settings, "keyint_sec", 2);
+
+	obs_encoder_update(vencoder, vencoder_settings);
+
+	obs_encoder_set_video(vencoder, obs_get_video());
+
+	// Configure audio encoder
+	obs_data_t *aencoder_settings = obs_data_create();
+
+	obs_data_set_int(aencoder_settings, "bitrate", 128);
+
+	obs_encoder_update(aencoder, aencoder_settings);
+
+	obs_encoder_set_audio(aencoder, obs_get_audio());
+
+	// Configure service
+
+	obs_data_set_string(service_settings, "service", "rtmp_custom");
+	obs_data_set_string(service_settings, "server", serverUrl.c_str());
+	obs_data_set_string(service_settings, "key", streamKey.c_str());
+
+	obs_data_set_bool(service_settings, "use_auth", useAuth);
+	obs_data_set_string(service_settings, "username", authUsername.c_str());
+	obs_data_set_string(service_settings, "password", authPassword.c_str());
+
+	obs_service_update(service, service_settings);
+	obs_service_apply_encoder_settings(service, vencoder_settings,
+					   aencoder_settings);
+
+	// Configure output
+
+	const char *output_type = obs_service_get_output_type(service);
+
+	if (!output_type)
+		output_type = "rtmp_output";
+
+	obs_data_t *output_settings = obs_data_create();
+
+	if (bindToIP.size()) {
+		obs_data_set_string(output_settings, "bind_ip", bindToIP.c_str());
+	}
+
+	m_output = obs_output_create(output_type, "test_stream",
+				     output_settings, nullptr);
+
+	obs_output_set_video_encoder(m_output, vencoder);
+	obs_output_set_audio_encoder(m_output, aencoder, 0);
+
+	obs_output_set_service(m_output, service);
+
+	obs_data_release(output_settings);
+	obs_data_release(service_settings);
+	obs_data_release(vencoder_settings);
+	obs_data_release(aencoder_settings);
+
+	obs_output_start(m_output);
+
+	return true;
+}
--- streamelements/StreamElementsObsOutputManager.hpp
+++ streamelements/StreamElementsObsOutputManager.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <obs.h>
+#include <obs-frontend-api.h>
 //#include <media-io/video-io.h>
 
+#include <mutex>
 #include <memory>
 #include <string>
 
@@ -16,10 +18,15 @@ public:
 	~StreamElementsObsOutputManager() {}
 
 public:
-	void DeserializeOutput(CefRefPtr<CefValue> input,
-			       CefRefPtr<CefValue> output);
+	//void DeserializeOutput(CefRefPtr<CefValue> input,
+	//		       CefRefPtr<CefValue> output);
+
+	std::string AddSceneOutput(obs_scene_t *scene, int view_width, int view_height);
+	bool StartOutputById(std::string id);
+	bool StopOutputById(std::string id);
 
 private:
+	std::recursive_mutex m_mutex;
 	std::map<std::string, std::shared_ptr<StreamElementsObsOutput>>
 		m_outputs;
 };
@@ -27,13 +34,67 @@ private:
 
 class StreamElementsObsOutput {
 public:
-	StreamElementsObsOutput() {}
-	~StreamElementsObsOutput() {}
+	StreamElementsObsOutput(obs_scene_t *scene, int view_width,
+				int view_height)
+	{
+		m_container_scene = obs_scene_create_private(
+			GetComputerSystemUniqueId().c_str());
+
+		obs_source_t *source_to_render =
+			scene ? obs_scene_get_source(scene)
+			      : obs_frontend_get_current_scene();
+
+		obs_sceneitem_t *item =
+			obs_scene_add(m_container_scene, source_to_render);
+
+		// Get container scene source
+		obs_source_t *container_source =
+			obs_scene_get_source(m_container_scene);
+
+		// Calculate aspect ratio
+		double aspect_ratio =
+			(double)view_width / (double)view_height;
+
+		int width = obs_source_get_width(source_to_render);
+		int height = obs_source_get_height(source_to_render);
+
+		// TODO: Calculate crop
+
+		// Create crop filter
+		obs_data_t *crop_settings = obs_data_create();
+
+		obs_data_set_bool(crop_settings, "relative", false);
+		obs_data_set_int(crop_settings, "left", 200);
+		obs_data_set_int(crop_settings, "top", 0);
+		obs_data_set_int(crop_settings, "right", 200);
+		obs_data_set_int(crop_settings, "bottom", 0);
+		obs_data_set_int(crop_settings, "cx", 0);
+		obs_data_set_int(crop_settings, "cy", 0);
+
+		obs_source_t *crop_filter_source = obs_source_create(
+			"crop_filter", "Crop", crop_settings, nullptr);
+
+		obs_data_release(crop_settings);
+
+		obs_source_filter_add(container_source, crop_filter_source);
+	}
+
+	~StreamElementsObsOutput()
+	{
+		Stop();
+
+		obs_scene_release(m_container_scene);
+	}
 
 public:
-	bool Start(video_t* video) {
-		auto video_info = video_output_get_info(video);
-		//video_info->
-	}
+	bool Start();
 	void Stop();
+
+private:
+	std::recursive_mutex m_mutex;
+	obs_scene_t *m_container_scene = nullptr;
+	obs_view_t *m_view = nullptr;
+	video_t *m_video = nullptr;
+
+	obs_output_t *m_output = nullptr;
 };
