03622817feb250db135cbb693915023f8b08bd53
Ilya Melamed <ilya77@gmail.com>
2024-06-21 14:51:17 +0300

    wip

---
+++ streamelements/StreamElementsComposition.cpp
@@ -0,0 +1,60 @@
+#include "StreamElementsComposition.hpp"
+#include <obs-frontend-api.h>
+
+////////////////////////////////////////////////////////////////////////////////
+// OBS Main Composition
+////////////////////////////////////////////////////////////////////////////////
+
+class StreamElementsDefaultCompositionInfo
+	: public StreamElementsCompositionBase::StreamElementsCompositionInfo {
+public:
+	StreamElementsDefaultCompositionInfo(
+		StreamElementsCompositionBase *owner)
+		: StreamElementsCompositionBase::StreamElementsCompositionInfo(
+			  owner)
+	{
+	}
+
+	virtual ~StreamElementsDefaultCompositionInfo() {}
+
+public:
+	virtual obs_encoder_t *GetStreamingVideoEncoder() {
+		auto output = obs_frontend_get_streaming_output();
+
+		auto result = obs_output_get_video_encoder(output);
+
+		obs_output_release(output);
+
+		return result;
+	}
+
+	virtual obs_encoder_t *GetStreamingAudioEncoder(size_t index) {
+		auto output = obs_frontend_get_streaming_output();
+
+		auto result = obs_output_get_audio_encoder(output, index);
+
+		obs_output_release(output);
+
+		return result;
+	}
+
+	virtual video_t *GetVideo() { return obs_get_video(); }
+	virtual audio_t *GetAudio() { return obs_get_audio(); }
+};
+
+std::shared_ptr<
+	StreamElementsCompositionBase::StreamElementsCompositionInfo>
+StreamElementsDefaultComposition::GetCompositionInfo()
+{
+	return std::make_shared<StreamElementsDefaultCompositionInfo>(this);
+}
+
+void StreamElementsDefaultComposition::SerializeComposition(
+	CefRefPtr<CefValue> &output)
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// TODO: OBS Views
+////////////////////////////////////////////////////////////////////////////////
+
---
+++ streamelements/StreamElementsComposition.hpp
@@ -0,0 +1,115 @@
+#pragma once
+
+#include <obs.h>
+#include "cef-headers.hpp"
+
+class StreamElementsCompositionBase {
+public:
+	class StreamElementsCompositionInfo {
+	private:
+		StreamElementsCompositionBase *m_owner;
+
+	public:
+		StreamElementsCompositionInfo(
+			StreamElementsCompositionBase *owner)
+			: m_owner(owner)
+		{
+			m_owner->AddRef();
+		}
+
+		virtual ~StreamElementsCompositionInfo() {
+			m_owner->RemoveRef();
+		}
+
+	public:
+		virtual obs_encoder_t *GetStreamingVideoEncoder() = 0;
+		virtual obs_encoder_t *
+		GetStreamingAudioEncoder(size_t index) = 0;
+
+		virtual video_t *GetVideo() = 0;
+		virtual audio_t *GetAudio() = 0;
+	};
+
+private:
+	size_t m_refCounter = 0;
+	std::mutex m_refCounterMutex;
+
+	void AddRef()
+	{
+		std::lock_guard<decltype(m_refCounterMutex)> lock(
+			m_refCounterMutex);
+
+		++m_refCounter;
+	}
+
+	void RemoveRef() {
+		std::lock_guard<decltype(m_refCounterMutex)> lock(
+			m_refCounterMutex);
+
+		--m_refCounter;
+	}
+
+public:
+	virtual bool CanRemove()
+	{
+		std::lock_guard<decltype(m_refCounterMutex)> lock(
+			m_refCounterMutex);
+
+		return m_refCounter == 0;
+	}
+
+	virtual std::shared_ptr<StreamElementsCompositionInfo>
+	GetCompositionInfo() = 0;
+
+private:
+	std::string m_id;
+	std::string m_name;
+
+protected:
+	StreamElementsCompositionBase(const std::string name, const std::string id): m_id(id), m_name(name) {}
+	virtual ~StreamElementsCompositionBase() {}
+
+public:
+	std::string GetId() { return m_id; }
+	std::string GetName() { return m_name; }
+
+public:
+	virtual void SerializeComposition(CefRefPtr<CefValue> &output) = 0;
+};
+
+// OBS Main Composition
+class StreamElementsDefaultComposition : public StreamElementsCompositionBase,
+	  public std::enable_shared_from_this<
+		  StreamElementsDefaultComposition> {
+private:
+	struct Private {
+		explicit Private() = default;
+	};
+
+public:
+	// ctor only usable by this class
+	StreamElementsDefaultComposition(Private)
+		: StreamElementsCompositionBase("Default", "default")
+	{
+
+	}
+
+	virtual ~StreamElementsDefaultComposition() {
+	}
+
+public:
+	static std::shared_ptr<StreamElementsCompositionBase> Create()
+	{
+		return std::make_shared<StreamElementsDefaultComposition>(
+			Private());
+	}
+
+public:
+	virtual std::shared_ptr<
+		StreamElementsCompositionBase::StreamElementsCompositionInfo>
+	GetCompositionInfo();
+
+	virtual bool CanRemove() { return false; }
+
+	virtual void SerializeComposition(CefRefPtr<CefValue> &output);
+};
