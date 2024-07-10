f72f188429a013a1db678e70225d0bb54d0ec95a
Ilya Melamed <ilya77@gmail.com>
2024-07-10 12:39:02 +0300

    wip

---
+++ streamelements/StreamElementsVideoCompositionViewWidget.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "StreamElementsVideoCompositionManager.hpp"
+#include <QWidget>
+
+class StreamElementsVideoCompositionViewWidget : public QWidget, public StreamElementsCompositionEventListener
+{
+private:
+	std::shared_ptr<StreamElementsVideoCompositionBase> m_videoComposition;
+	std::shared_ptr<StreamElementsVideoCompositionBase::CompositionInfo> m_videoCompositionInfo;
+
+	obs_display_t *m_display;
+
+public:
+	StreamElementsVideoCompositionViewWidget(
+		QWidget *parent,
+		std::shared_ptr<StreamElementsVideoCompositionBase>
+			videoComposition);
+	~StreamElementsVideoCompositionViewWidget();
+
+private:
+	void CreateDisplay();
+
+	void OnMove();
+	void OnDisplayChange();
+
+protected:
+	virtual void paintEvent(QPaintEvent *event) override;
+	virtual void moveEvent(QMoveEvent *event) override;
+	virtual void resizeEvent(QResizeEvent *event) override;
+	virtual QPaintEngine *paintEngine() const override;
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+	virtual bool nativeEvent(const QByteArray &, void *message,
+				 qintptr *) override;
+#else
+	virtual bool nativeEvent(const QByteArray &, void *message,
+				 long *) override;
+#endif
+
+private:
+	static void obs_display_draw_callback(void *data, uint32_t cx,
+					      uint32_t cy);
+};
