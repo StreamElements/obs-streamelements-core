23b6a17f1fbf624cddfe765e28a12b8ff775ee93
Ilya Melamed <ilya77@gmail.com>
2024-07-17 12:25:16 +0300

    wip

--- streamelements/StreamElementsVideoCompositionViewWidget.cpp
+++ streamelements/StreamElementsVideoCompositionViewWidget.cpp
@@ -4,6 +4,68 @@
 #include <QMouseEvent>
 #include <QColor>
 
+static void
+scanGroupSceneItems(obs_sceneitem_t *group,
+		    std::function<void(obs_sceneitem_t * /*item*/)> callback,
+		    bool recursive)
+{
+	struct data_t {
+		std::function<void(obs_sceneitem_t * /*item*/)> callback;
+		bool recursive;
+	};
+
+	data_t data = {};
+	data.callback = callback;
+	data.recursive = recursive;
+
+	obs_sceneitem_group_enum_items(
+		group,
+		[](obs_scene_t *scene, obs_sceneitem_t *item, void *data_p) {
+			auto data = (data_t *)data_p;
+
+			data->callback(item);
+
+			if (data->recursive && obs_sceneitem_is_group(item)) {
+				scanGroupSceneItems(item, data->callback, data->recursive);
+			}
+
+			return true;
+		},
+		&data);
+}
+
+static void scanSceneItems(
+	obs_scene_t* scene,
+	std::function<void(obs_sceneitem_t * /*item*/)> callback,
+	bool recursive)
+{
+	struct data_t {
+		std::function<void(obs_sceneitem_t * /*item*/)> callback;
+		bool recursive;
+	};
+
+	data_t data = {};
+	data.callback = callback;
+	data.recursive = recursive;
+
+	obs_scene_enum_items(
+		scene,
+		[](obs_scene_t * /* scene */, obs_sceneitem_t *item,
+		   void *data_p) {
+			auto data = (data_t *)data_p;
+
+			data->callback(item);
+
+			if (data->recursive && obs_sceneitem_is_group(item)) {
+				scanGroupSceneItems(item, data->callback,
+						    data->recursive);
+			}
+
+			return true;
+		},
+		&data);
+}
+
 //
 // Define a projection region for drawing a view texture.
 //
