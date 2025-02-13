a128314aa49cdf8ae8b56b00e356fadaacee8628
Ilya Melamed <ilya77@gmail.com>
2025-02-13 15:41:59 +0300

    wip

---
+++ deps/BugSplatMac.framework/Headers/BugSplatMac.h
@@ -0,0 +1,15 @@
+//
+//  BugSplatMac.h
+//  BugSplat
+//
+//  Copyright © 2024 BugSplat, LLC. All rights reserved.
+//
+
+#ifndef BugSplatMac_h
+#define BugSplatMac_h
+
+#import <BugSplatMac/BugSplat.h>
+#import <BugSplatMac/BugSplatDelegate.h>
+#import <BugSplatMac/BugSplatAttachment.h>
+
+#endif /* BugSplatMac_h */
