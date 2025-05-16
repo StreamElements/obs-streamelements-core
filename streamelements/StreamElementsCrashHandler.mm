6782623c8f9ba8e7dcee99631b9ec618f1325049
Ilya Melamed <ilya77@gmail.com>
2025-05-16 12:55:20 +0300

    wip

--- streamelements/StreamElementsCrashHandler.mm
+++ streamelements/StreamElementsCrashHandler.mm
@@ -64,6 +64,459 @@
 //@import BugSplatMac;
 //#import <BugSplatMac/BugSplat.h>
 
+#include "cef-headers.hpp"
+#include "StreamElementsUtils.hpp"
+#include "StreamElementsConfig.hpp"
+#include <util/base.h>
+#include <util/platform.h>
+#include <util/config-file.h>
+#include <obs-frontend-api.h>
+#include <time.h>
+#include <cstdio>
+#include <string>
+#include <ios>
+#include <fstream>
+#include "deps/zip/zip.h"
+#include <iostream>
+#include <filesystem>
+#include <stdio.h>
+#include <fcntl.h>
+#include <condition_variable>
+#include <QUrl>
+
+/* ================================================================= */
+
+static config_t *obs_fe_user_config()
+{
+	auto config = StreamElementsConfig::GetInstance();
+
+	if (config) {
+		return config->GetObsUserConfig();
+	}
+
+	return nullptr;
+}
+
+/* ================================================================= */
+
+unsigned int s_maxRemainingLogFilesCount = 7;
+
+/* ================================================================= */
+
+static std::string GenerateTimeDateFilename(const char *extension,
+					    bool noSpace = false)
+{
+	time_t now = time(0);
+	char file[256] = {};
+	struct tm *cur_time;
+
+	cur_time = localtime(&now);
+	snprintf(file, sizeof(file), "%d-%02d-%02d%c%02d-%02d-%02d.%s",
+		 cur_time->tm_year + 1900, cur_time->tm_mon + 1,
+		 cur_time->tm_mday, noSpace ? '_' : ' ', cur_time->tm_hour,
+		 cur_time->tm_min, cur_time->tm_sec, extension);
+
+	return std::string(file);
+}
+
+static void delete_oldest_file(bool has_prefix, const char *location)
+{
+	UNUSED_PARAMETER(has_prefix);
+
+	char *basePathPtr = os_get_config_path_ptr(location);
+	std::string logDir(basePathPtr);
+	bfree(basePathPtr);
+
+	std::string oldestLog;
+	time_t oldest_ts = (time_t)-1;
+	struct os_dirent *entry;
+
+	os_dir_t *dir = os_opendir(logDir.c_str());
+	if (dir) {
+		unsigned int count = 0;
+
+		while ((entry = os_readdir(dir)) != NULL) {
+			if (entry->directory || *entry->d_name == '.')
+				continue;
+
+			std::string filePath =
+				logDir + "/" + std::string(entry->d_name);
+			struct stat st;
+			if (0 == os_stat(filePath.c_str(), &st)) {
+				time_t ts = st.st_ctime;
+
+				if (ts) {
+					if (ts < oldest_ts) {
+						oldestLog = filePath;
+						oldest_ts = ts;
+					}
+
+					count++;
+				}
+			}
+		}
+
+		os_closedir(dir);
+
+		if (count > s_maxRemainingLogFilesCount) {
+			os_unlink(oldestLog.c_str());
+		}
+	}
+}
+
+static void write_file_content(std::string &path, const char *content)
+{
+	std::fstream file;
+
+#ifdef _WIN32
+	std::wstring wpath = utf8_to_wstring(path);
+
+	file.open(wpath, std::ios_base::in | std::ios_base::out |
+				 std::ios_base::trunc | std::ios_base::binary);
+#else
+	file.open(path, std::ios_base::in | std::ios_base::out |
+				std::ios_base::trunc | std::ios_base::binary);
+#endif
+	file << content;
+
+	file.close();
+}
+
+/* ================================================================= */
+
+static std::string GetTemporaryFolderPath() {
+	char* folder_path = obs_module_file("crashes/current");
+	std::string result = folder_path;
+	bfree(folder_path);
+	
+	os_mkdirs(result.c_str());
+	
+	return result;
+}
+
+static bool GetTemporaryFilePath(const char *basename,
+					const char *ext,
+					std::string &tempBufPath)
+{
+	tempBufPath = GetTemporaryFolderPath() + basename + std::string(ext);
+	
+	return true;
+}
+
+static inline void AddObsConfigurationFiles()
+{
+	const size_t BUF_LEN = 2048;
+
+	std::string tempBufPath;
+	if (!GetTemporaryFilePath("selive-error-report-data", ".zip",
+				  tempBufPath))
+		return;
+
+	char programDataPathBuf[BUF_LEN];
+	int ret = os_get_config_path(programDataPathBuf, BUF_LEN, "obs-studio");
+
+	if (ret <= 0) {
+		return;
+	}
+
+	std::wstring obsDataPath = QString(programDataPathBuf).toStdWString();
+
+	zip_t *zip = zip_open(tempBufPath.c_str(), 9, 'w');
+
+	if (!zip) {
+		return;
+	}
+
+	auto addBufferToZip = [&](const char *buf, size_t bufLen,
+				  std::wstring zipPath) {
+		zip_entry_open(zip, wstring_to_utf8(zipPath).c_str());
+
+		zip_entry_write(zip, buf, bufLen);
+
+		zip_entry_close(zip);
+	};
+
+	auto addLinesBufferToZip = [&](std::vector<std::string> &lines,
+				       std::wstring zipPath) {
+		zip_entry_open(zip, wstring_to_utf8(zipPath).c_str());
+
+		for (auto line : lines) {
+			zip_entry_write(zip, line.c_str(), line.size());
+			zip_entry_write(zip, "\r\n", 2);
+		}
+
+		zip_entry_close(zip);
+	};
+
+	auto addCefValueToZip = [&](CefRefPtr<CefValue> &input,
+				    std::wstring zipPath) {
+		std::string buf = wstring_to_utf8(
+			CefWriteJSON(input, JSON_WRITER_PRETTY_PRINT)
+				.ToWString());
+
+		zip_entry_open(zip, wstring_to_utf8(zipPath).c_str());
+
+		zip_entry_write(zip, buf.c_str(), buf.size());
+
+		zip_entry_close(zip);
+	};
+
+	auto addFileToZip = [&](std::string localPath, std::wstring zipPath) {
+		int fd = open(localPath.c_str(), O_RDONLY,
+				 0, 0 /*_S_IREAD | _S_IWRITE*/);
+
+		if (-1 != fd) {
+			size_t BUF_LEN = 32768;
+
+			char *buf = new char[BUF_LEN];
+
+			zip_entry_open(zip, wstring_to_utf8(zipPath).c_str());
+
+			int read_count = read(fd, buf, BUF_LEN);
+			while (read_count > 0) {
+				if (0 != zip_entry_write(zip, buf, read_count)) {
+					break;
+				}
+
+				read_count = read(fd, buf, BUF_LEN);
+			}
+
+			zip_entry_close(zip);
+
+			delete[] buf;
+
+			close(fd);
+		} else {
+			// Failed opening file for reading
+			//
+			// This is a crash handler: you can't really do anything
+			// here to mitigate.
+		}
+	};
+
+	std::string package_manifest = "generator=crash_handler\nversion=4\n";
+	addBufferToZip(package_manifest.c_str(),
+		       package_manifest.size(), L"manifest.ini");
+
+	std::map<std::string, std::wstring> local_to_zip_files_map;
+
+	// Collect files
+	std::vector<std::string> blacklist = {
+		"plugin_config/obs-streamelements/obs-streamelements-update.exe",
+		"plugin_config/obs-browser/cache/",
+		"plugin_config/obs-browser/blob_storage/",
+		"plugin_config/obs-browser/code cache/",
+		"plugin_config/obs-browser/gpucache/",
+		"plugin_config/obs-browser/visited links/",
+		"plugin_config/obs-browser/transportsecurity/",
+		"plugin_config/obs-browser/videodecodestats/",
+		"plugin_config/obs-browser/session storage/",
+		"plugin_config/obs-browser/service worker/",
+		"plugin_config/obs-browser/pepper data/",
+		"plugin_config/obs-browser/indexeddb/",
+		"plugin_config/obs-browser/file system/",
+		"plugin_config/obs-browser/databases/",
+		"plugin_config/obs-browser/obs-streamelements-core.ini.bak",
+		"plugin_config/obs-browser/cef.",
+		"plugin_config/obs-browser/obs_profile_cookies/",
+		"plugin_config/obs-streamelements-core/cache/",
+		"plugin_config/obs-streamelements-core/blob_storage/",
+		"plugin_config/obs-streamelements-core/code cache/",
+		"plugin_config/obs-streamelements-core/gpucache/",
+		"plugin_config/obs-streamelements-core/visited links/",
+		"plugin_config/obs-streamelements-core/transportsecurity/",
+		"plugin_config/obs-streamelements-core/videodecodestats/",
+		"plugin_config/obs-streamelements-core/session storage/",
+		"plugin_config/obs-streamelements-core/service worker/",
+		"plugin_config/obs-streamelements-core/pepper data/",
+		"plugin_config/obs-streamelements-core/indexeddb/",
+		"plugin_config/obs-streamelements-core/file system/",
+		"plugin_config/obs-streamelements-core/databases/",
+		"plugin_config/obs-streamelements-core/obs-streamelements-core.ini.bak",
+		"plugin_config/obs-streamelements-core/cef.",
+		"plugin_config/obs-streamelements-core/obs_profile_cookies/",
+		"plugin_config/obs-streamelements-core/crashes/",
+		"updates/",
+		"profiler_data/",
+		"obslive_restored_files/",
+		"plugin_config/obs-browser/streamelements_restored_files/",
+		"plugin_config/obs-streamelements-core/streamelements_restored_files/",
+		"crashes/"};
+
+	// Collect all files
+	for (auto &i :
+	     std::filesystem::recursive_directory_iterator(
+		     programDataPathBuf)) {
+		if (!std::filesystem::is_directory(i.path())) {
+			std::string local_path = i.path().string();
+			std::wstring zip_path =
+				utf8_to_wstring(local_path).substr(obsDataPath.size() + 1);
+
+			std::wstring zip_path_lcase = zip_path;
+			std::transform(zip_path_lcase.begin(),
+				       zip_path_lcase.end(),
+				       zip_path_lcase.begin(), ::towlower);
+			std::transform(zip_path_lcase.begin(),
+				       zip_path_lcase.end(),
+				       zip_path_lcase.begin(), [](wchar_t ch) {
+					       return ch == L'\\' ? L'/' : ch;
+				       });
+
+			bool accept = true;
+			for (auto item : blacklist) {
+				if (zip_path_lcase.size() >= item.size()) {
+					if (zip_path_lcase.substr(
+						    0, item.size()) == utf8_to_wstring(item)) {
+						accept = false;
+
+						break;
+					}
+				}
+			}
+
+			if (accept) {
+				local_to_zip_files_map[local_path] =
+					L"obs-studio/" + zip_path;
+			}
+		}
+	}
+
+	for (auto item : local_to_zip_files_map) {
+		addFileToZip(item.first, item.second);
+	}
+
+	{
+		CefRefPtr<CefValue> basicInfo = CefValue::Create();
+		CefRefPtr<CefDictionaryValue> d = CefDictionaryValue::Create();
+		basicInfo->SetDictionary(d);
+
+		d->SetString("obsVersion", obs_get_version_string());
+		d->SetString("cefVersion", GetCefVersionString());
+		d->SetString("cefApiHash", GetCefPlatformApiHash());
+
+#ifdef WIN32
+		d->SetString("platform", "windows");
+#elif defined(__APPLE__)
+		d->SetString("platform", "macos");
+#elif defined(__linux__)
+		d->SetString("platform", "linux");
+#endif
+
+		if (sizeof(void*) == 8) {
+			d->SetString("platformArch", "64bit");
+		} else {
+			d->SetString("platformArch", "32bit");
+		}
+
+		d->SetString("streamelementsPluginVersion",
+			     GetStreamElementsPluginVersionString());
+
+		d->SetString("machineUniqueId", GetComputerSystemUniqueId());
+
+		addCefValueToZip(basicInfo, L"system\\basic.json");
+	}
+
+	GetAsyncCallContextStack([&](const StreamElementsAsyncCallContextStack_t
+					     *asyncCallContextStack) {
+		if (!asyncCallContextStack->size())
+			return;
+
+		std::vector<std::string> lines;
+
+		lines.push_back("Asynchronous call context:");
+		lines.push_back("");
+
+		for (auto item : *asyncCallContextStack) {
+			char buf[32];
+			sprintf(buf, "%d", item->line);
+			
+			lines.push_back(
+				item->file.c_str() + std::string(" (line: ") +
+				std::string(buf) + ")");
+		}
+
+		addLinesBufferToZip(lines, L"async_context.txt");
+
+		// Fill in async call details notes
+
+		auto asyncContextList = CefListValue::Create();
+
+		for (auto item : *asyncCallContextStack) {
+			auto d = CefDictionaryValue::Create();
+
+			d->SetString("file", item->file);
+			d->SetInt("line", item->line);
+			d->SetInt("thread", item->thread);
+			d->SetBool("running", item->running);
+
+			asyncContextList->SetDictionary(
+				asyncContextList->GetSize(), d);
+		}
+				// Retrieve temporary path
+		std::string wtempBufPath;
+		if (!GetTemporaryFilePath("async-context", ".json",
+					  wtempBufPath))
+			return;
+
+		auto asyncContextListValue = CefValue::Create();
+
+		asyncContextListValue->SetList(asyncContextList);
+
+		auto json = CefWriteJSON(asyncContextListValue,
+					 JSON_WRITER_PRETTY_PRINT)
+				    .ToString();
+
+		os_quick_write_utf8_file(wtempBufPath.c_str(),
+					 json.c_str(), json.size(), false);
+	});
+
+	zip_close(zip);
+
+	////////////////////////////////////////////////////////////////////
+	// Process ApiContext
+	////////////////////////////////////////////////////////////////////
+
+	GetApiContext([](StreamElementsApiContext_t *apiContext) {
+		auto apiContextList = CefListValue::Create();
+
+		int apiContextIndex = 0;
+		for (auto api : *apiContext) {
+			auto apiContextListItem = CefDictionaryValue::Create();
+
+			apiContextListItem->SetString("invoke",
+						      api.get()->method);
+			apiContextListItem->SetList("invokeArgs",
+						    api.get()->args);
+			apiContextListItem->SetInt("thread", api.get()->thread);
+
+			apiContextList->SetDictionary(apiContextList->GetSize(),
+						      apiContextListItem);
+
+			++apiContextIndex;
+		}
+
+		// Retrieve temporary path
+		std::string wtempBufPath;
+		if (!GetTemporaryFilePath("api-context", ".json",
+					  wtempBufPath))
+			return;
+
+		auto apiContextListValue = CefValue::Create();
+
+		apiContextListValue->SetList(apiContextList);
+
+		auto json = CefWriteJSON(apiContextListValue,
+					 JSON_WRITER_PRETTY_PRINT)
+				    .ToString();
+
+		os_quick_write_utf8_file(wtempBufPath.c_str(),
+					 json.c_str(), json.size(), false);
+	});
+}
+
+/* ================================================================= */
+
+
 static bool AmIBeingDebugged(void)
     // Returns true if the current process is being debugged (either
     // running under the debugger or has a debugger attached post facto).
@@ -105,6 +558,8 @@ static void TrackCrash(const char* caller_reference)
     if (!initialized) return;
 
     blog(LOG_ERROR, "obs-streamelements-core: StreamElements: Crash Handler: TrackCrash: %s", caller_reference);
+	
+    AddObsConfigurationFiles();
 
     // This will also report the platform and a bunch of other props
     StreamElementsGlobalStateManager::GetInstance()
