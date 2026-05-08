cc30803992f504edb51e46f7012d609a3c90de76
Ilya Melamed <ilya77@gmail.com>
2026-05-08 20:54:38 +0300

    wip

--- streamelements/StreamElementsVideoCompositionManager.cpp
+++ streamelements/StreamElementsVideoCompositionManager.cpp
@@ -168,11 +168,6 @@ void StreamElementsVideoCompositionManager::DeserializeComposition(
 			    videoFrame->GetType("height") != VTYPE_INT)
 				return;
 
-			blog(LOG_INFO, "[obs-streamelements-core]: ----- StreamElementsVideoCompositionManager::DeserializeComposition(): streamingVideoEncoders: %s",
-			     CefWriteJSON(streamingVideoEncoders,
-					  JSON_WRITER_PRETTY_PRINT)
-				     .c_str());
-
 			composition =
 				StreamElementsCustomVideoComposition::Create(
 					id, name, videoFrame->GetInt("width"),
